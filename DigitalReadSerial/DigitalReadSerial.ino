f926d12df3c8d8d1b59443c2f9b33a24da59890c
Thellman <02cathellman@gmail.com>
2025-04-30 14:05:05 -0400

    Left IS Left and up IS up
    Wokring

--- DigitalReadSerial/DigitalReadSerial.ino
+++ DigitalReadSerial/DigitalReadSerial.ino
@@ -6,48 +6,69 @@
  * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-joystick
  */
 
-#include <ezButton.h>
-
 #define VRX_PIN  A1 // Arduino pin connected to VRX pin
 #define VRY_PIN  A0 // Arduino pin connected to VRY pin
-#define SW_PIN   2  // Arduino pin connected to SW  pin
 
-ezButton button(SW_PIN);
+#define LEFT_THRESHOLD  400
+#define RIGHT_THRESHOLD 800
+#define UP_THRESHOLD    400
+#define DOWN_THRESHOLD  800
 
-int xValue = 0; // To store value of the X axis
-int yValue = 0; // To store value of the Y axis
-int bValue = 0; // To store value of the button
+#define COMMAND_NO     0x00
+#define COMMAND_RIGHT 0x01
+#define COMMAND_DOWN  0x02
+#define COMMAND_LEFT    0x04
+#define COMMAND_UP  0x08
+
+int xValue = 0 ; // To store value of the X axis
+int yValue = 0 ; // To store value of the Y axis
+int command = COMMAND_NO;
 
 void setup() {
   Serial.begin(9600) ;
-  button.setDebounceTime(300); // set debounce time to 50 milliseconds
 }
 
 void loop() {
-  button.loop(); // MUST call the loop() function first
-
   // read analog X and Y analog values
-  yValue = -analogRead(VRX_PIN);
-  xValue = -analogRead(VRY_PIN);
+  xValue = analogRead(VRX_PIN);
+  yValue = analogRead(VRY_PIN);
 
-  // Read the button value
-  bValue = button.getState();
+  // converts the analog value to commands
+  // reset commands
+  command = COMMAND_NO;
 
-  if (button.isPressed()) {
-    Serial.println("The button is pressed");
-    // TODO do something here
+  // check left/right commands
+  if (xValue < LEFT_THRESHOLD)
+    command = command | COMMAND_LEFT;
+  else if (xValue > RIGHT_THRESHOLD)
+    command = command | COMMAND_RIGHT;
+
+  // check up/down commands
+  if (yValue < UP_THRESHOLD)
+    command = command | COMMAND_UP;
+  else if (yValue > DOWN_THRESHOLD)
+    command = command | COMMAND_DOWN;
+
+  // NOTE: AT A TIME, THERE MAY BE NO COMMAND, ONE COMMAND OR TWO COMMANDS
+
+  // print command to serial and process command
+  if (command & COMMAND_LEFT) {
+    Serial.println("COMMAND UP");
+    // TODO: add your task here
   }
 
-  if (button.isReleased()) {
-    Serial.println("The button is released");
-    // TODO do something here
+  if (command & COMMAND_RIGHT) {
+    Serial.println("COMMAND DOWN");
+    // TODO: add your task here
   }
 
-  // print data to Serial Monitor on Arduino IDE
-  Serial.print("x = ");
-  Serial.print(xValue);
-  Serial.print(", y = ");
-  Serial.print(yValue);
-  Serial.print(" : button = ");
-  Serial.println(bValue);
+  if (command & COMMAND_UP) {
+    Serial.println("COMMAND RIGHT");
+    // TODO: add your task here
+  }
+
+  if (command & COMMAND_DOWN) {
+    Serial.println("COMMAND LEFT");
+    // TODO: add your task here
+  }
 }
